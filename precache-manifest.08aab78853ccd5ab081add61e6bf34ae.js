808b25db6e140aa2947f702c46cdbf067b6b0e98
unknown <shiesh1766.cse@chitkara.edu.in>
2020-02-29 20:38:17 +0530

    Updates

---
+++ precache-manifest.08aab78853ccd5ab081add61e6bf34ae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54daadc936d7e919e69b0ea60ef90da4",
+    "url": "/shivesh947/portfolio/index.html"
+  },
+  {
+    "revision": "ef4e0e29d317ea2ddeab",
+    "url": "/shivesh947/portfolio/static/css/2.06565635.chunk.css"
+  },
+  {
+    "revision": "c16f4c1f39ea15f830ad",
+    "url": "/shivesh947/portfolio/static/css/main.454c074f.chunk.css"
+  },
+  {
+    "revision": "ef4e0e29d317ea2ddeab",
+    "url": "/shivesh947/portfolio/static/js/2.01116be3.chunk.js"
+  },
+  {
+    "revision": "c16f4c1f39ea15f830ad",
+    "url": "/shivesh947/portfolio/static/js/main.d62ec5bf.chunk.js"
+  },
+  {
+    "revision": "b810c0cfe15ed8a75e2b",
+    "url": "/shivesh947/portfolio/static/js/runtime-main.7c66104f.js"
+  },
+  {
+    "revision": "3824aa7bef858e7cc530d6555718747c",
+    "url": "/shivesh947/portfolio/static/media/aboutme.3824aa7b.svg"
+  },
+  {
+    "revision": "a580ff242843a799457b6bf6235da4fb",
+    "url": "/shivesh947/portfolio/static/media/advancetraininglogo.a580ff24.svg"
+  },
+  {
+    "revision": "4718cbc6aa778f1fde04d2bbf654377b",
+    "url": "/shivesh947/portfolio/static/media/projectlogo.4718cbc6.svg"
+  },
+  {
+    "revision": "0ab47ba903a83cfd6beb579b8bc43f92",
+    "url": "/shivesh947/portfolio/static/media/resume.0ab47ba9.pdf"
+  },
+  {
+    "revision": "164e25a6ffe61ffbf631d4fab35acbf6",
+    "url": "/shivesh947/portfolio/static/media/skilllogo.164e25a6.svg"
+  }
+]);
